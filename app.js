ba37b02bfb1dae45ffa074ea8073ce2baade20c3
Umaima Salam <umaimasalam2003@gmail.com>
2025-07-05 20:33:27 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,49 @@
+// Question 1: Variable and Math Expressions
+/*let x = 10;
+let y = 5;
+console.log("x = 10 , y = 5");
+console.log("x + y =" , x + y);
+console.log("x - y =" , x - y);
+console.log("x * y =" , x * y);
+console.log("x / y =" , x / y);*/
+
+
+//Question 2: if...else Statement and Comparision Operators
+/*let age = prompt("Enter your age: ");
+if(age>=18){
+    alert("You're an adult")
+}
+else{
+    alert("You're a minor")
+}*/
+
+
+// Question 3: Arrays and for Loops:
+// let fruits = ["Apple" , "Banana" , "Cherry"];
+// for(let i = 0 ; i<fruits.length; i++){
+// console.log(fruits[i]);
+// }
+
+
+// Question 4: Concatenating Text Strings and Alerts
+// let firstName = "John";
+// let lastName = " Doe";
+// alert("Hello, " + firstName + lastName);
+
+
+// Question 5: Nested if Statements and Boolean Logic
+/*let isAdmin = true;
+let isLoggedIn = true;
+if(isAdmin){
+    if(isLoggedIn){
+        console.log("Welcome, admin!");  
+    }
+    else {
+        console.log("Kindly, Log In your account");  
+    }
+}
+else{
+    console.log("Please, Signup your account");
+    
+}*/
+
